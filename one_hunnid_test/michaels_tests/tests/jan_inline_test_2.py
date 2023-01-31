d9873a730c65765907221d4e0dfeaa00366f0b19
Jan C. Rubio <61097870+jcr4698@users.noreply.github.com>
2023-01-31 14:25:08 -0600

    Jan Inline Test 1 and 2: Regex Tests
    1.) Regex-based implementation of the split() method
    2.) Check id number with regex

--- one_hunnid_test/michaels_tests/tests/jan_inline_test_2.py
+++ one_hunnid_test/michaels_tests/tests/jan_inline_test_2.py
@@ -16,15 +16,16 @@ from inline import Here
 #simulate input
 input_list = sys.argv[1:]
 
-# generate an identification number
-id_num = ""
-poss_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'E', 'I', 'O', 'U', 'X', 'Y']
-for digit in range(6):
-    id_num = id_num + str(poss_digits[random.randint(0, 15)])
-print(id_num)
+for iter in range(10):
+    # generate an identification number
+    id_num = ""
+    poss_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'E', 'I', 'O', 'U', 'X', 'Y']
+    for digit in range(6):
+        id_num = id_num + str(poss_digits[random.randint(0, 15)])
+    print(id_num)
 
-# expected answer
-res = True if re.match(r"[^BCDFGHJKLMNPQRSTVWZ]{6}", id_num) else False
+    # expected answer
+    res = True if re.match(r"[^BCDFGHJKLMNPQRSTVWZ]{6}", id_num) else False
 
-# verify
-Here().given(input_list, poss_digits).check_true(res)
+    # verify
+    Here().given(input_list, poss_digits).check_true(res)
