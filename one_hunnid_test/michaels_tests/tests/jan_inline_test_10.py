4a70ba04cbe1224e1f9aa4ca554ae542b7ca277b
Jan C. Rubio <61097870+jcr4698@users.noreply.github.com>
2023-02-01 13:49:45 -0600

    Jan Inline Tests 1-11
    New string manipulation tests

---
+++ one_hunnid_test/michaels_tests/tests/jan_inline_test_10.py
@@ -0,0 +1,30 @@
+# Inline Test # 10
+# Senior Design Project - FH12
+# Author: Jan C. Rubio
+# Date: 2/1/2023
+
+import sys
+import re
+import random
+from inline import Here
+
+# type: string manipulation
+
+# purpose: change the string to upper-case
+
+# simulate input
+input_list = sys.argv[1:]
+
+for iter in range(10):
+    word = ""
+    res = ""
+    for n in range(5):
+        # generate word
+        ch_idx = random.randint(0, 14)
+        word += chr(97 + ch_idx)
+        res += chr(65 + ch_idx)
+    
+    print(word + " -> " + res)
+
+    # verify
+    Here().given(input_list, word).check_eq(res, word.upper())
---
+++ one_hunnid_test/michaels_tests/tests/jan_inline_test_11.py
@@ -0,0 +1,27 @@
+# Inline Test # 11
+# Senior Design Project - FH12
+# Author: Jan C. Rubio
+# Date: 2/1/23
+
+import sys
+import re
+import random
+from inline import Here
+
+# type: string manipulation
+
+# purpose: make a string into a palindrome
+
+# simulate input
+input_list = sys.argv[1:]
+
+for iter in range(10):
+    word = ""
+    for n in range(3):
+        # generate word
+        ch_idx = random.randint(0, 25)
+        word = chr(97 + ch_idx) + word + chr(97 + ch_idx)
+    print(word[0:3] + " => " + word)
+
+    # verify
+    Here().given(input_list, word).check_eq(word, word[0:3] + "".join(reversed(word[0:3])))
