3f71fa57cf8f96c65bbe75e2d104c3d4bee239fc
brandinguyen <91094031+brandinguyen@users.noreply.github.com>
2023-02-16 15:45:24 -0600

    Create test_10.py

---
+++ test_classification/bit_manipulation/test_10.py
@@ -0,0 +1,30 @@
+from inline import Here
+
+def count_set_bits(n):
+    count = 0
+    while n:
+        count += n & 1
+        n >>= 1
+    return count
+
+def toggle_kth_bit(n, k):
+    return n ^ (1 << (k - 1))
+
+def main():
+    num = 5
+    print(bin(num)) # binary representation of num
+    print("Number of set bits:", count_set_bits(num))
+    # hardcode the inputs and outputs
+    new_num = toggle_kth_bit(num, 2)
+    # Here().given(num_before, 5).check_eq(new_num, 7)
+    new_num_1 = toggle_kth_bit(num, 4)
+    new_num_2 = toggle_kth_bit(num, 1)
+    new_num_3 = toggle_kth_bit(num, 3)
+    Here().given(num, 9).check_eq(new_num_3, 13)
+    for i in range(10):
+        new_num_2 = toggle_kth_bit(num, 1)
+    print(bin(num)) # binary representation after toggling 2nd bit
+    print("Number of set bits:", count_set_bits(num))
+
+if __name__ == '__main__':
+    main()
