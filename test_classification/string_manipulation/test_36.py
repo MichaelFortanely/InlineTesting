00643cf4bd28d88e5cc3b1e8a8cf66562116652a
sydthompson <sydneythompson@utexas.edu>
2023-02-16 16:10:58 -0600

    Adding last tests to classification.

---
+++ test_classification/string_manipulation/test_36.py
@@ -0,0 +1,48 @@
+# https://github.com/qiyuangong/leetcode/blob/master/python/005_Longest_Palindromic_Substring.py
+
+from inline import Here
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # my solution
+        # expand string according to Manacher algorithm
+        # but extend radius step by step
+        ls = len(s)
+        if ls <= 1 or len(set(s)) == 1:
+            return s
+        # create a new list like this: "abc"->"a#b#c"
+        temp_s = '#'.join('{}'.format(s))
+        Here().given(s, "aAbcD").check_eq(temp_s, "a#A#b#c#D#")
+        
+        # print temp_s
+        tls = len(temp_s)
+        seed = range(1, tls - 1)
+        # this table stores the max length palindrome
+        len_table = [0] * tls
+        for step in range(1, tls / 2 + 1):
+            final = []
+            for pos in seed:
+                if pos - step < 0 or pos + step >= tls:
+                    continue
+                if temp_s[pos - step] != temp_s[pos + step]:
+                    continue
+                final.append(pos)
+                if temp_s[pos - step] == '#':
+                    continue
+                len_table[pos] = step
+            seed = final
+        max_pos, max_step = 0, 0
+        for i, s in enumerate(len_table):
+            if s >= max_step:
+                max_step = s
+                max_pos = i
+        return temp_s[max_pos - max_step:max_pos + max_step + 1].translate(None, '#')
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print(s.longestPalindrome("abcbe"))
---
+++ test_classification/string_manipulation/test_38.py
@@ -0,0 +1,24 @@
+from inline import Here
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        # iterative solution
+        res = [[]]
+        for i in range(len(nums)):
+            cache = set()
+            while len(res[0]) == i:
+                curr = res.pop(0)
+                for j in range(len(curr) + 1):
+                    # generate new n permutations from n -1 permutations
+                    new_perm = curr[:j] + [nums[i]] + curr[j:]
+                    print(new_perm)
+                    stemp = ''.join(map(str, new_perm))
+                    Here().given(new_perm, ['1', 's', '7', '4']).check_eq(stemp, '1s74')
+                    
+                    print(stemp)
+                    if stemp not in cache:
+                        cache.add(stemp)
+                        res.append(new_perm)
+        return res
+
+Solution.permuteUnique("l", '1234')
