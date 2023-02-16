ee9f9e1aebec46a86412b836c4057cf880736d65
brandinguyen <91094031+brandinguyen@users.noreply.github.com>
2023-02-16 15:34:09 -0600

    Create test_18.py

---
+++ test_classification/collection_manipulation/test_18.py
@@ -0,0 +1,20 @@
+from inline import Here
+from math import inf 
+  
+def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+        # inline test here
+        Here().given(word2, "ow").given(word1, "wo").check_eq(dp, [[inf, inf, inf], [inf, inf, inf], [inf, inf, inf]])
+
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
+
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
