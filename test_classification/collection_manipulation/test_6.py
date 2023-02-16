11f32a9c0792e45313d46f90f42853832bd775ff
brandinguyen <91094031+brandinguyen@users.noreply.github.com>
2023-02-16 15:02:16 -0600

    Create test_6.py

---
+++ test_classification/collection_manipulation/test_6.py
@@ -0,0 +1,36 @@
+import sys
+from inline import Here
+
+#Source: NeetCode(Linked List Cycle)
+#Classification: LinkedLists
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+#Example Case:
+Root = ListNode(1)
+Root.next = ListNode(2)
+Root.next.next = Root
+
+Root2 = ListNode(1)
+Root2.next = ListNode(2)
+Root2.next.next = ListNode(3)
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                x = slow
+                return True
+        return False
+    
+obj = Solution()
+
+ans = obj.hasCycle(Root) 
+Here().given(Root, ListNode(1)).check_eq(ans, False)
