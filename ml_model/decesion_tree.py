94aaf9abad1088b42619ac3776415ff0cc91fc76
sydthompson <sydneythompson@utexas.edu>
2023-03-22 22:15:44 -0500

    Adding branches to np array

---
+++ ml_model/decesion_tree.py
@@ -0,0 +1,32 @@
+import find_features
+
+import pandas as pd
+import numpy as np
+
+def run_tree():
+    arr=np.load('simple_data.npy')
+    x=np.ndarray.tolist(arr)
+    print(len(x))
+    print(x[0])
+    print(len(x[0]))
+
+    x[0].extend(['is_keyword', 'length', 'is_regular_expression', 'is_mathematical_calculation',
+            'is_collection_manipulation', 'is_string_manipulation', 'is_bit_manipulation','is_variable_assignment'])
+    
+    for i in range(1,len(x)-1):
+        item=x[i]
+        line=item[1]
+        keyword=find_features.is_keyword(line)
+        length=find_features.length(line)
+        regex=find_features.is_regular_expression(line)
+        math=find_features.is_mathematical_calculation(line)
+        coll=find_features.is_collection_manipulation(line)
+        str_manipulate=find_features.is_string_manipulation(line)
+        bit=find_features.is_bit_manipulation(line)
+        var=find_features.is_variable_assignment(line)
+
+        item.extend([keyword, length, regex, math, coll, str_manipulate, bit, var])
+
+    print(x[0])
+
+    return np.array(x)
--- ml_model/parsing.py
+++ ml_model/parsing.py
@@ -5,11 +5,9 @@ import pandas as pd
 
 def create_set():
     lines = []
-    lines.append(["line_number", "line", "isLOI"])
-
     # print(os.getcwd())
     # print(os.listdir())
-
+    lines.append(['line_number', 'line','isLOI'])
     file_path=os.getcwd() + "/test_classification"
     dir_list= os.listdir(file_path)
     for file_name in dir_list:
@@ -17,9 +15,10 @@ def create_set():
         file_list = os.listdir(current_path)
         for file in file_list:
             lines=process_lines(current_path + "\\" + file, lines)
-    arr=np.array(lines)
-    pd.DataFrame(arr).to_csv("inline_test_set.csv")
-
+    arr=np.asarray(lines)
+    print(arr.shape)
+    np.save('simple_data.npy', arr)
+ 
 
 
 def process_user_file(file_name, lines) -> list:
@@ -29,7 +28,7 @@ def process_user_file(file_name, lines) -> list:
         current_line = x[i].strip()
         current_line, skip=process_comment(current_line)
 
-        if not skip and "Here()." not in current_line:
+        if (not skip) and ("Here(" not in current_line) and (len(current_line > 0)):
             lines.append([i, current_line, 0])
     file.close()
     return lines
@@ -42,15 +41,15 @@ def process_lines(file_name, lines) -> list:
         next_line = x[i+1]
         current_line, skip=process_comment(current_line)
 
-        if not skip and "Here()." in next_line and not "Here()." in current_line:
-            lines.append([i, current_line, 1])
-        elif not skip and "Here()." in current_line:
+        if skip or "Here(" in current_line or (len(current_line) == 0):
             pass
-        elif not skip:
+        elif "Here(" in next_line:
+            lines.append([i, current_line, 1])
+        else:
             lines.append([i, current_line, 0])
     if len(x) > 0:
         current_line, skip=process_comment(x[len(x)-1])
-        if not "Here()." in current_line and not skip:
+        if (not "Here(" in current_line) and (not skip):
             lines.append([len(x) - 1, current_line, 0])
     file.close()
     return lines
