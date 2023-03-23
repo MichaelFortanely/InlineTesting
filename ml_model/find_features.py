fa70eba2edb0a676dd8c84840b083ec186cfc45a
brandinguyen <91094031+brandinguyen@users.noreply.github.com>
2023-03-22 23:29:39 -0500

    Update find_features.py

--- ml_model/find_features.py
+++ ml_model/find_features.py
@@ -1,44 +1,41 @@
 def is_keyword(line):
-    if line.contains('if' or 'elif' or 'else' or 'for' or 'while' or 'break' or
+    if 'if' or 'elif' or 'else' or 'for' or 'while' or 'break' or
     'continue' or 'else' or 'def' or 'pass' or 'lambda' or 'return' or 'yield' or 
     'import' or 'from' or 'as' or 'try' or 'except' or 'raise' or 'finally' or 'else'
-    or 'assert' or 'async' or 'await' or 'del' or 'global' or 'nonlocal'):
-        return 0
-    else:
+    or 'assert' or 'async' or 'await' or 'del' or 'global' or 'nonlocal') in line:
         return 1
+    else:
+        return 0
 
 def length(line):
     return line.length()
 
 def is_regular_expression(line):
-    if line.contains('re.'):
+    if ('re.') in line:
         return 1
     else:
         return 0
 
 def is_mathematical_calculation(line):
-    if line.contains('-' or '+' or '*' or '/' or 'math.'):
+    if '-' or '+' or '*' or '/' or 'math.' in line:
         return 1
     else:
         return 0 
 
 def is_collection_manipulation(line):
-    if line.contains('append ' or 'clear' or 'copy' or 'extend' or 
-                    'insert' or 'pop' or 'remove' or 'reverse' or
-                    'sort'):
+    if 'append ' or 'clear' or 'copy' or 'extend' or 'insert' or 'pop' or 'remove' or 'reverse' or 'sort' in line:
         return 1
     else:
         return 0
 
 def is_string_manipulation(line):
-    if line.contains('capitalize' or 'casefold' or 'center' or 'encode'
-                    or 'format' or 'join' or 'lower' or 'replace'):
+    if 'capitalize' or 'casefold' or 'center' or 'encode' or 'format' or 'join' or 'lower' or 'replace' in line:
         return 1
     else:
         return 0
 
 def is_bit_manipulation(line):
-    if line.contains('&' or '|' or '^' or '~' or '<<' or '>>'):
+    if '&' or '|' or '^' or '~' or '<<' or '>>' in line:
         return 1
     else:
         return 0
