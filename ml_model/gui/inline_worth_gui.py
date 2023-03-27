cea81641ff4db61601c762bcaf0bc76635570d35
Jan <janrubio@utexas.edu>
2023-03-27 16:48:10 -0500

    gui has ability to open a text file and paste in text box

--- ml_model/gui/inline_worth_gui.py
+++ ml_model/gui/inline_worth_gui.py
@@ -8,10 +8,10 @@ class InlineGUI:
         self.root = tk.Tk()
 
         ''' size and color prefs '''
-        self.root.geometry("500x500")
-        self.root.title("Determine Inline Test Worthiness")
+        self.root.geometry("800x500")
+        self.root.title("Inline Test Worthiness")
 
-        label = tk.Label(self.root, text="Hello, World!")
+        label = tk.Label(self.root, text="Determine whether a Program Statement is worthy of an Inline Test!", font=("Arial", 16))
         label.pack(padx=20, pady=20)
 
         ''' functionality '''
@@ -27,7 +27,10 @@ class InlineGUI:
         self.root.config(menu=self.menu_bar)
 
         # user text input
-        
+        self.text_box = tk.Text(self.root, height=13, font=("Arial", 16))
+        self.text_box.pack(padx=10, pady=10)
+        self.submit_btn = tk.Button(self.root, text="submit", font=("Arial", 16))
+        self.submit_btn.pack(padx=10, pady=10)
 
         # exit
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -40,8 +43,19 @@ class InlineGUI:
         if messagebox.askyesno(title="Quit?", message="Are you sure you want to close window?"):
             self.root.destroy()
 
-    """ Open user file from 'user_files' directory. """
+    """ Print user file from 'user_files' directory into text field. """
     def open_file(self):
         self.root.filename = filedialog.askopenfile(initialdir="../../user_files", title="Select a file...", filetypes=(("Python Files", "*.py"), ("Text Files", "*.txt")))
+        file_dir = open(self.root.filename.name, "r")
+        file_txt = file_dir.read()
+        if(len(self.text_box.get("1.0", tk.END)) == 1):
+            self.text_box.insert("1.0", file_txt)
+        else:
+            if messagebox.askyesno(title="There is text in the text box...", message="Do you want to append it?\nYes - Append\nNo - Replace"):
+                self.text_box.insert(tk.END, "\n"+file_txt)
+            else:
+                self.text_box.delete("1.0", "end")
+                self.text_box.insert("1.0", file_txt)
+
 
 InlineGUI()
