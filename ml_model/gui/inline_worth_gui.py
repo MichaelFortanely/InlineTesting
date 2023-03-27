2fa0748da110f4462717dac1d1ae7aac700536ee
Jan <janrubio@utexas.edu>
2023-03-27 01:59:57 -0500

    added option to open user file (does nothing yet)

--- ml_model/gui/inline_worth_gui.py
+++ ml_model/gui/inline_worth_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 
 class InlineGUI:
 
@@ -18,10 +18,16 @@ class InlineGUI:
 
         # menu bar
         self.menu_bar = tk.Menu(self.root)
-        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(label="Close", command=self.on_closing)
-        self.menu_bar.add_cascade(menu = self.file_menu, label="File")
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0) # create bar
+
+        self.file_menu.add_command(label="Close", command=self.on_closing)  # close button
+        self.file_menu.add_command(label="Open File", command=self.open_file) # open user file
+
+        self.menu_bar.add_cascade(menu = self.file_menu, label="File")  # name of bar option
         self.root.config(menu=self.menu_bar)
+
+        # user text input
+        
 
         # exit
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -33,5 +39,9 @@ class InlineGUI:
     def on_closing(self):
         if messagebox.askyesno(title="Quit?", message="Are you sure you want to close window?"):
             self.root.destroy()
+
+    """ Open user file from 'user_files' directory. """
+    def open_file(self):
+        self.root.filename = filedialog.askopenfile(initialdir="../../user_files", title="Select a file...", filetypes=(("Python Files", "*.py"), ("Text Files", "*.txt")))
 
 InlineGUI()
